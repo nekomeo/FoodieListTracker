ffb40dcfc48cd8cf5660a46b1b92bbb0e90e9950
Elle Ti <linh.htu@gmail.com>
2020-02-09 15:11:45 -0800

    Initial project creation & repo setup

---
+++ FoodieListTracker/FoodieListTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodieListTracker
+//
+//  Created by Elle Ti on 2020-02-09.
+//  Copyright © 2020 Elle Ti. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
